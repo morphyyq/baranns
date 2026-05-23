fb61cd6270a04171aa50db5f7ec0302dc66c2d75
morphyyq <georgy.bzykoff@gmail.com>
2026-05-23 21:16:04 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,16 +9,27 @@ const commands = [
             option.setName('text')
                 .setDescription('Сообщение')
                 .setRequired(true)
-        )
+        ),
+
+    // ✅ ДОБАВИЛИ PANEL
+    new SlashCommandBuilder()
+        .setName('panel')
+        .setDescription('Отправить панель заявок в семью')
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-    await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
-        { body: commands }
-    );
+    try {
+        console.log('⏳ Registering slash commands...');
 
-    console.log('✅ Slash commands registered');
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands }
+        );
+
+        console.log('✅ Slash commands registered');
+    } catch (err) {
+        console.log(err);
+    }
 })();
